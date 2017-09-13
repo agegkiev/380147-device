1a1afc0ce9f7d114013db90e424df75a723b0c87
Aznaur Gegkiev <gegkiev@bk.ru>
2017-09-13 11:45:58 +0300

    1

--- js/script.js
+++ js/script.js
@@ -2,21 +2,29 @@ var link = document.querySelector(".btn-write-us");
 var popup = document.querySelector(".write-us");
 var close = popup.querySelector(".modal-close");
 
-var mapbtn = document.querySelector(".map-mini");
+var maplink = document.querySelector(".map-mini");
 var popupmap = document.querySelector(".map");
 var closemap = popupmap.querySelector(".modal-close-map");
+
+var login = popup.querySelector("[name=login]");
+
+var form = popup.querySelector("form");
+
+var email = popup.querySelector("[name=email]");
 
 link.addEventListener("click", function (evt) {
     evt.preventDefault();
     popup.classList.add("write-us-show");
+    login.focus();
 });
 
 close.addEventListener("click", function (evt) {
     evt.preventDefault();
     popup.classList.remove("write-us-show");
+    popup.classList.remove("modal-error");
 });
 
-mapbtn.addEventListener("click", function (evt) {
+maplink.addEventListener("click", function (evt) {
     evt.preventDefault();
     popupmap.classList.add("map-show");
 });
@@ -25,3 +33,24 @@ closemap.addEventListener("click", function (evt) {
     evt.preventDefault();
     popupmap.classList.remove("map-show");
 });
+
+form.addEventListener("submit", function (evt) {
+    evt.preventDefault() 
+    {
+        if (!login.value || !email.value) 
+    {
+      evt.preventDefault();
+      popup.classList.add("modal-error");
+    }
+    }
+  });
+
+window.addEventListener("keydown", function (evt) {
+    if (evt.keyCode === 27) {
+      if (popup.classList.contains("write-us-show")) {
+          popup.classList.remove("write-us-show");
+          popup.classList.remove("modal-error");
+      }
+    }
+  });
+
